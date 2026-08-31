05a94dd1018a2992d0193f4aa04d42b8d0fd89ba
Diego Córdova <diego.cordova24@inacapmail.cl>
2026-08-31 13:39:03 -0400

    31-08-2026

--- inventario/models.py
+++ inventario/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
 class Producto(models.Model):
     nombre = models.CharField(max_length=64)
     precio = models.IntegerField(default=0)
--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'inventario',
     'bienvenida',
     'django.contrib.admin',
     'django.contrib.auth',
