5405d15166fe64372603b1e02e3c540982f114f8
Diego Córdova <diego.cordova24@inacapmail.cl>
2026-09-01 13:29:27 -0400

    01-09-2026

--- inventario/models.py
+++ inventario/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Producto(models.Model):
-    nombre = models.CharField(max_length=64)
-    precio = models.IntegerField(default=0)
-    descripcion = models.CharField(max_length=128, null=True, blank=True)
-    stock = models.IntegerField(default=0)
+    nombre= models.CharField(max_length=64)
+    precio= models.IntegerField(default=0)
+    descripcion= models.CharField(max_length=128, blank=True, null=True)
+    stock= models.IntegerField(default=0)
--- inventario/views.py
+++ inventario/views.py
@@ -2,35 +2,38 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Producto
 from .forms import ProductoForm
 
+from django.views.decorators.csrf import csrf_protect
+
 # Create your views here.
 def producto_list(request):
     productos = Producto.objects.all()
-    return render(request,'inventario/producto_list.html',{'object_list':productos})
+    return render(request, 'inventario/producto_list.html', {'object_list': productos})
 
 def producto_detail(request, pk):
-    producto = get_object_or_404(Producto,pk=pk)
-    return render(request,'inventario/producto_detail.html',{'object':producto})
+    producto = get_object_or_404(Producto, pk=pk)
+    return render(request, 'inventario/producto_detail.html', {'object': producto})
 
+@csrf_protect
 def producto_create(request):
     if request.method == 'POST':
         form = ProductoForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('producto_List')
+            return redirect('producto_list')
 
     else:
         form = ProductoForm()
-        return render(request, 'inventario/producto_form.html',{'form':form}) 
+        return render(request, 'inventario/producto_form.html', {'form': form})
 
 def producto_update(request, pk):
-    producto = get_object_or_404(Producto, pk=pk) 
+    producto = get_object_or_404(Producto, pk=pk)
 
     if request.method == 'POST':
-        form = ProductoForm(request.POST, instance=producto) 
+        form = ProductoForm(request.POST, instance=producto)
 
         if form.is_valid():
             form.save()
-            return redirect('producto_lisy') 
+            return redirect('producto_list')
 
     else:
         form = ProductoForm(instance=producto)
@@ -43,6 +46,4 @@ def producto_delete(request, pk):
         producto.delete()
         return redirect('producto_list')
 
-    return render(request, 'inventario/producto_confirm_delete.html',{'object':producto})
-
-    
+    return render(request, 'inventario/producto_confirm_delete.html', {'object': producto})
